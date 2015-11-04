04d031e5502c9b9d0c4f42452c7e9c783364d499
peter willsey <pwillsey@yammer-inc.com>
2015-11-04 14:00:38 -0800

    Add license to some files that were missing it.

--- OAuthSDK/YMAPIClient.h
+++ OAuthSDK/YMAPIClient.h
@@ -1,3 +1,4 @@
+//
 // YMAPIClient.h
 //
 // Copyright (c) 2015 Microsoft
@@ -19,6 +20,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
+//
 
 #import <Foundation/Foundation.h>
 #import <AFNetworking/AFNetworking.h>
--- OAuthSDK/YMAPIClient.m
+++ OAuthSDK/YMAPIClient.m
@@ -1,3 +1,4 @@
+//
 // YMAPIClient.m
 //
 // Copyright (c) 2015 Microsoft
@@ -19,6 +20,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
+//
 
 #import "YMLoginClient.h"
 #import "YMAPIClient.h"
